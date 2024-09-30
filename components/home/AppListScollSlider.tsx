0542588500b5a2d55c1fd1b9344442601b44c5ca
Farhaan mahbub <39917360+Farhan-meb@users.noreply.github.com>
2024-09-30 22:52:04 +0600

    Update AppListScollSlider.tsx

--- components/home/AppListScollSlider.tsx
+++ components/home/AppListScollSlider.tsx
@@ -24,8 +24,24 @@ const HorizontalScrollCarousel = () => {
     const x = useTransform(scrollYProgress, [0, 0.9], ["1%", "-82%"]);
 
     return (
-        <section ref={targetRef} className="relative h-[300vh]">
+        <section ref={targetRef} className="relative h-[300vh] mt-20 lg:mt-40">
             <div className="sticky top-0 left-0 flex h-screen items-center overflow-hidden">
+                <motion.div
+                    className="absolute inset-0 z-0"
+                    style={{
+                        backgroundColor: useTransform(
+                            scrollYProgress,
+                            [0, 0.25, 0.5, 0.75, 1],
+                            [
+                                "rgba(26, 54, 54, 0)",
+                                "rgba(30, 32, 30, 0)",
+                                "rgba(26, 54, 54, 0)",
+                                "rgba(30, 32, 30, 0)",
+                                "rgba(26, 54, 54, 0)",
+                            ]
+                        ),
+                    }}
+                />
                 <motion.div style={{ x }} className="flex gap-20">
                     {cards.map((card, index) => {
                         return (
@@ -45,7 +61,7 @@ const HorizontalScrollCarousel = () => {
 const Card = ({ card, isLast }: { card: Card; isLast: boolean }) => {
     return (
         <div
-            className={`flex items-center gap-10 max-w-[1400px] mx-auto ${
+            className={`flex  items-center gap-10 max-w-[1400px] mx-auto ${
                 isLast ? "mr-[50vw]" : ""
             }`}
         >
@@ -64,10 +80,10 @@ const Card = ({ card, isLast }: { card: Card; isLast: boolean }) => {
                 </div>
             </div>
             <motion.div className="flex flex-col gap-4">
-                <p className="text-6xl bg-clip-text text-transparent bg-gradient-to-r from-[#2856cb]  via-[#F56028] to-[#FAA22B] uppercase font-semibold max-w-[400px] break-words hyphens-auto">
+                <p className="text-6xl bg-clip-text text-transparent bg-gradient-to-r from-[#2856cb]  via-[#F56028] to-[#FAA22B] font-bold max-w-[400px] break-words hyphens-auto">
                     {card.label}
                 </p>
-                <p className="text-xl font-medium text-gray-600 dark:text-gray-400">
+                <p className="text-lg font-medium text-gray-600 dark:text-gray-400">
                     {card.description}
                 </p>
             </motion.div>
