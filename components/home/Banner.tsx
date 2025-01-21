d35b469d7f0aca077969e166342d0f41bc1ffdd4
Farhaan mahbub <39917360+Farhan-meb@users.noreply.github.com>
2025-01-21 22:57:23 +0600

    Update Banner.tsx

--- components/home/Banner.tsx
+++ components/home/Banner.tsx
@@ -39,7 +39,7 @@ export default function Banner() {
                     />
                 </div>
             </div>
-            <div className="absolute bottom-[-150px] left-1/2 transform -translate-x-1/2 rounded-2xl border border-white shadow">
+            <div className="absolute bottom-[-180px] left-1/2 transform -translate-x-1/2 rounded-2xl border border-white shadow">
                 <div className="bg-white rounded-2xl  md:w-[700px] m-2 shadow-[1px_-30px_100px_50px_rgba(187,_0,_255,_0.2)]">
                     <HeroBannerList />
                     {/* <Image
