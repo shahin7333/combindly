2cfb279f4ef76360c030e19d4d959a6b194322bc
Farhaan mahbub <39917360+Farhan-meb@users.noreply.github.com>
2025-01-26 02:02:26 +0600

    Update HeroBottomBannerList.tsx

--- components/hero-section/HeroBottomBannerList.tsx
+++ components/hero-section/HeroBottomBannerList.tsx
@@ -1,10 +1,10 @@
-import { HeroBannerList } from "../home/BannerList";
+import { BannerList } from "../home/BannerList";
 
 function HeroBottomBannerList() {
     return (
         <div className="absolute bottom-[-180px] left-1/2 transform -translate-x-1/2 rounded-2xl border border-white shadow">
             <div className="bg-white rounded-2xl  md:w-[700px] m-2 shadow-[1px_-30px_100px_50px_rgba(187,_0,_255,_0.2)]">
-                <HeroBannerList />
+                <BannerList />
             </div>
         </div>
     );
