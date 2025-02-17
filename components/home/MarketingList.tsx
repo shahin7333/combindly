2f2bb2ef1db7225a1a92249f091db8e188b0df7a
shahintechnext <shahin.technext@gmail.com>
2025-02-17 22:57:40 +0600

    update pricing card

--- components/home/MarketingList.tsx
+++ components/home/MarketingList.tsx
@@ -4,121 +4,114 @@ import image1 from "@/app/assets/atc_white_banner.png";
 import { BannerList } from "./BannerList";
 
 const marketingList = [
-    {
-        title: "Boost Sales and Revenue.",
+  {
+    title: "Boost Sales and Revenue.",
+    description:
+      "Optimize your store with 8+ tools designed to enhance conversions and improve customer experience.",
+    description_points: [
+      {
+        title: "Drive Conversions:",
         description:
-            "Optimize your store with 8+ tools designed to enhance conversions and improve customer experience.",
-        description_points: [
-            {
-                title: "Drive Conversions:",
-                description:
-                    "Features like Stock Scarcity, Countdown Timers, and Sticky Add to Cart create urgency, encouraging faster purchases.",
-            },
-            {
-                title: "Enhance Engagement:",
-                description:
-                    "Recently Viewed Products, Related Products, and Shipping Info keep customers engaged and informed.",
-            },
-            {
-                title: "Seamless Integration:",
-                description:
-                    "Easy to integrate with Shopify, offering a fully customizable experience for merchants.",
-            },
-            {
-                title: "Enhance Store Performance",
-                description:
-                    "Optimized to streamline shopping journeys and elevate overall store performance.",
-            },
-        ],
-        badges: [
-            "🛒 Always Accessible",
-            "🚀 Instant Conversions",
-            "🎨 Customizable Design",
-            "⚙️ Effortless Integration",
-        ],
-        image: image1,
-        position: "normal",
-    },
-    // {
-    //     title: "Boost Conversions with Ease",
-    //     description:
-    //         "Streamline the shopping journey with our Sticky Add To Cart app. Elevate user experience effortlessly:",
-    //     badges: [
-    //         "🛒 Always Accessible",
-    //         "🚀 Instant Conversions",
-    //         "🎨 Customizable Design",
-    //         "⚙️ Effortless Integration",
-    //     ],
-    //     image: image1,
-    //     position: "reverse",
-    // },
+          "Features like Stock Scarcity, Countdown Timers, and Sticky Add to Cart create urgency, encouraging faster purchases.",
+      },
+      {
+        title: "Enhance Engagement:",
+        description:
+          "Recently Viewed Products, Related Products, and Shipping Info keep customers engaged and informed.",
+      },
+      {
+        title: "Seamless Integration:",
+        description:
+          "Easy to integrate with Shopify, offering a fully customizable experience for merchants.",
+      },
+      {
+        title: "Enhance Store Performance",
+        description:
+          "Optimized to streamline shopping journeys and elevate overall store performance.",
+      },
+    ],
+    badges: [
+      "🛒 Always Accessible",
+      "🚀 Instant Conversions",
+      "🎨 Customizable Design",
+      "⚙️ Effortless Integration",
+    ],
+    image: image1,
+    position: "normal",
+  },
+  // {
+  //     title: "Boost Conversions with Ease",
+  //     description:
+  //         "Streamline the shopping journey with our Sticky Add To Cart app. Elevate user experience effortlessly:",
+  //     badges: [
+  //         "🛒 Always Accessible",
+  //         "🚀 Instant Conversions",
+  //         "🎨 Customizable Design",
+  //         "⚙️ Effortless Integration",
+  //     ],
+  //     image: image1,
+  //     position: "reverse",
+  // },
 ];
 
 function MarketingList() {
-    return (
-        <div className="max-w-[1440px] mx-auto px-4 lg:px-16 font-axiforma py-10 md:py-20">
-            <SectionHeader
-                title="Built for Shopify, Tailored to your needs"
-                description="Experience a seamless boost in conversion rates with our
+  return (
+    <div className="max-w-[1440px] mx-auto px-4 lg:px-16 font-axiforma py-10 md:py-20">
+      <SectionHeader
+        title="Built for Shopify, Tailored to your needs"
+        description="Experience a seamless boost in conversion rates with our
                     all-in-one app collection – simplifying success for you."
-            />
-            <div className="max-w-[1200px] mx-auto mt-10 md:mt-[100px]">
-                {marketingList.map((item, index) => (
-                    <div
-                        key={index}
-                        className={`flex gap-10 relative ${
-                            item.position === "reverse"
-                                ? "flex-row-reverse"
-                                : ""
-                        }`}
-                    >
-                        <div className="flex-1 max-w-[42%]">
-                            <h2
-                                className="gradient-text text-xl md:text-[41px] font-extrabold"
-                                style={{
-                                    lineHeight: "1",
-                                }}
-                            >
-                                {item.title}
-                            </h2>
-                            <p className="text-md md:text-[19px] text-[#24223E] dark:text-white mt-6 font-semibold">
-                                {item.description}
-                            </p>
-                            <div className="mt-6">
-                                {item.description_points.map((point, index) => (
-                                    <div
-                                        key={index}
-                                        className="flex gap-3 mb-4"
-                                    >
-                                        <span className="text-md md:text-[16px] font-semibold gradient-text">
-                                            ✔️
-                                        </span>
-                                        <h3 className="text-md md:text-[16px] text-[#24223E] dark:text-white max-w-[450px]">
-                                            <span className="font-semibold">
-                                                {point.title}
-                                            </span>{" "}
-                                            <span>{point.description}</span>
-                                        </h3>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                        <div
-                            className={`absolute top-0 ${
-                                item.position === "reverse"
-                                    ? " -left-40 pr-6 rounded-r-xl  shadow-[-2px_50px_100px_50px_rgba(255,_149,_0,_0.1)]"
-                                    : "-right-40 pl-6 rounded-l-xl gradient-bg"
-                            } pt-6 `}
-                        >
-                            <div className="-mb-10">
-                                <BannerList className="rounded-l-2xl h-[400px]" />
-                            </div>
-                        </div>
-                    </div>
+      />
+      <div className="max-w-[1200px] mx-auto mt-10 md:mt-[100px]">
+        {marketingList.map((item, index) => (
+          <div
+            key={index}
+            className={`flex gap-10 relative ${
+              item.position === "reverse" ? "flex-row-reverse" : ""
+            }`}
+          >
+            <div className="flex-1 max-w-[42%]">
+              <h2
+                className="gradient-text text-xl md:text-[41px] font-extrabold"
+                style={{
+                  lineHeight: "1",
+                }}
+              >
+                {item.title}
+              </h2>
+              <p className="text-md md:text-[19px] text-[#24223E] dark:text-white mt-6 font-semibold">
+                {item.description}
+              </p>
+              <div className="mt-6">
+                {item.description_points.map((point, index) => (
+                  <div key={index} className="flex gap-3 mb-4">
+                    <span className="text-md md:text-[16px] font-semibold gradient-text">
+                      ✔️
+                    </span>
+                    <h3 className="text-md md:text-[16px] text-[#24223E] dark:text-white max-w-[450px]">
+                      <span className="font-semibold">{point.title}</span>{" "}
+                      <span>{point.description}</span>
+                    </h3>
+                  </div>
                 ))}
+              </div>
             </div>
-        </div>
-    );
+            <div
+              className={`absolute top-0 ${
+                item.position === "reverse"
+                  ? " -left-40 pr-6 rounded-r-xl  shadow-[-2px_50px_100px_50px_rgba(255,_149,_0,_0.1)]"
+                  : "-right-40 pl-6 rounded-l-xl gradient-bg"
+              } pt-6 `}
+            >
+              <div className="-mb-10">
+                <BannerList className="rounded-l-2xl h-[400px]" />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default MarketingList;
--- components/pricing/Pricing.tsx
+++ components/pricing/Pricing.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { BadgePercent } from "lucide-react";
+import { BadgeCheck, BadgePercent } from "lucide-react";
 import { Button } from "../ui/button";
 import {
   Card,
@@ -10,130 +10,220 @@ import {
   CardTitle,
 } from "../ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
+import { use, useState } from "react";
 
 export default function Pricing() {
+  const [cardData, setCardData] = useState([
+    {
+      title: "Advanced",
+      price: "$14.99",
+      duration: "per month",
+      features: [
+        {
+          title: "Animated Add To Cart",
+          description:
+            "Bring your products to life – where every click becomes a delightful experience.",
+        },
+        {
+          title: "Sticky Add To Cart",
+          description:
+            "Ensures your cart is always at your fingertips, ready for a seamless checkout experience!",
+        },
+        {
+          title: "Countdown",
+          description:
+            "Adds urgency to your deals, making every moment a must-buy opportunity.",
+        },
+        {
+          title: "Sticky Buy Now",
+          description:
+            "Instant satisfaction at your fingertips – for a seamless and swift checkout experience.",
+        },
+        {
+          title: "Shipping Infromation",
+          description:
+            "Navigate hassle-free deliveries – your go-to for clear and reliable shipping details!",
+        },
+        {
+          title: "Stock Scarcity",
+          description:
+            "Keeps you informed and boosts urgency for items running low.",
+        },
+        {
+          title: "Related Products",
+          description: "Discover more you'll love – meet your perfect match.",
+        },
+        {
+          title: "Recently Viewed Products",
+          description:
+            "Your browsing history at your fingertips to rediscover favorites.",
+        },
+        {
+          title: "24/7 Life time Support",
+        },
+        {
+          title: "Automated Updated Features",
+        },
+      ],
+      buttonText: "Start trail now",
+    },
+    {
+      title: "Premium",
+      price: "$149",
+      duration: "per year",
+      features: [
+        {
+          title: "Animated Add To Cart",
+          description:
+            "Bring your products to life – where every click becomes a delightful experience.",
+        },
+        {
+          title: "Sticky Add To Cart",
+          description:
+            "Ensures your cart is always at your fingertips, ready for a seamless checkout experience!",
+        },
+        {
+          title: "Countdown",
+          description:
+            "Adds urgency to your deals, making every moment a must-buy opportunity.",
+        },
+        {
+          title: "Sticky Buy Now",
+          description:
+            "Instant satisfaction at your fingertips – for a seamless and swift checkout experience.",
+        },
+        {
+          title: "Shipping Infromation",
+          description:
+            "Navigate hassle-free deliveries – your go-to for clear and reliable shipping details!",
+        },
+        {
+          title: "Stock Scarcity",
+          description:
+            "Keeps you informed and boosts urgency for items running low.",
+        },
+        {
+          title: "Related Products",
+          description: "Discover more you'll love – meet your perfect match.",
+        },
+        {
+          title: "Recently Viewed Products",
+          description:
+            "Your browsing history at your fingertips to rediscover favorites.",
+        },
+        {
+          title: "24/7 Life time Support",
+        },
+        {
+          title: "Automated Updated Features",
+        },
+      ],
+      buttonText: "Start trail now",
+    },
+  ]);
   return (
     <div className="max-w-[1440px] mx-auto px-4 lg:px-16 font-axiforma mt-[72px] mb-20">
-      <div className="mb-10">
-        <h1 className="text-3xl font-bold mb-3 text-center">
-          Plans and Pricing
+      <div className="text-center mb-10 lg:max-w-[60%] mx-auto">
+        <h1 className="text-xl md:text-[41px] font-extrabold text-[#24223E] dark:text-white md:leading-[50px]">
+          Affordable Pricing Plans for Combindly - Tailored to Fit Every Budget
         </h1>
-        <p className="text-center font-light">
-          Receive unlimited credits when you pay yearly, and save on your plan.
+        <p className="text-md md:text-[19px] font-semibold text-[#24223E] dark:text-white mt-6 mx-auto">
+          Explore cost-effective pricing for Combindly: Tailored plans for all
+          business sizes, offering maximum value with no hidden costs. Perfect
+          for budget-friendly, efficient solutions.
         </p>
       </div>
       <Tabs defaultValue="monthly">
         <div className="flex justify-center items-center gap-4 mb-10">
-          <TabsList className="flex justify-center items-center gap-4 bg-transparent max-w-max">
-            <TabsTrigger className="uppercase" value="monthly">
+          <TabsList className="flex justify-center items-center gap-4 bg-transparent max-w-max bg-gray-100 dark:bg-gray-800 rounded-full h-[42px]">
+            <TabsTrigger className="h-[35px]" value="monthly">
               Monthly
             </TabsTrigger>
-            <TabsTrigger className="uppercase" value="yearly">
-              Yearly
+            <TabsTrigger className="h-[35px]" value="yearly">
+              Yearly{" "}
+              <span className="text-xs px-2 py-1 bg-gray-200 dark:bg-gray-700 rounded-full ms-2">
+                Save 16.67%
+              </span>
             </TabsTrigger>
           </TabsList>
         </div>
         <TabsContent value="monthly">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
-            {[0, 1, 2, 4].map((item) => (
-              <Card key={item} className="hover:border-primary hover:shadow-lg">
-                <CardHeader>
-                  <CardTitle className="text-lg font-medium">
-                    Organizations
-                  </CardTitle>
-                  <CardDescription>
-                    <div className="mt-4">
-                      <p className="text-2xl font-semibold">$100</p>
-                      <p className="text-xs text-gray-500">Per Month/user</p>
+          <Card className="border-[#7877c6] border-2 max-w-[500px] mx-auto inset-0 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.1),rgba(255,255,255,0))] dark:bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+            <CardHeader>
+              <CardTitle className="text-xl font-medium">
+                {cardData[0].title}
+              </CardTitle>
+              <CardDescription>
+                <div className="mt-4">
+                  <p className="text-2xl font-semibold">{cardData[0].price}</p>
+                  <p className="text-xs text-gray-500">
+                    {cardData[0].duration}
+                  </p>
+                </div>
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="px-6">
+              <div>
+                <div className="space-y-3 mt-4 text-gray-700 dark:text-gray-300 font-light">
+                  {cardData[0].features.map((feature, index) => (
+                    <div key={index}>
+                      <p className="flex items-center gap-1.5 font-medium">
+                        <BadgeCheck className="h-5 w-5" />
+                        {feature.title}
+                      </p>
+                      {feature.description && (
+                        <p className="text-sm pl-6 pt-0.5">
+                          {feature.description}
+                        </p>
+                      )}
                     </div>
-                  </CardDescription>
-                </CardHeader>
-                <CardContent className="px-6">
-                  <div>
-                    <p className="text-sm font-medium text-gray-700">
-                      Great for large businesses
-                    </p>
-                    <div className="space-y-3 mt-4 text-gray-700 font-light">
-                      <p className="text-sm flex items-center gap-1.5">
-                        <BadgePercent className="h-5 w-5" />
-                        Great for large businesses
-                      </p>
-                      <p className="text-sm flex items-center gap-1.5">
-                        <BadgePercent className="h-5 w-5" />
-                        15 second checks
-                      </p>
-                      <p className="text-sm flex items-center gap-1.5">
-                        <BadgePercent className="h-5 w-5" />
-                        Single-user account
-                      </p>
-                      <p className="text-sm flex items-center gap-1.5">
-                        <BadgePercent className="h-5 w-5" />
-                        50 monitors
-                      </p>
-                      <p className="text-sm flex items-center gap-1.5">
-                        <BadgePercent className="h-5 w-5" />
-                        Up to 10 seats
-                      </p>
-                    </div>
-                  </div>
-                </CardContent>
-                <CardFooter className="mt-4">
-                  <Button className="w-full">Get started</Button>
-                </CardFooter>
-              </Card>
-            ))}
-          </div>
+                  ))}
+                </div>
+              </div>
+            </CardContent>
+            <CardFooter className="mt-4">
+              <Button className="w-full">{cardData[0].buttonText}</Button>
+            </CardFooter>
+          </Card>
         </TabsContent>
         <TabsContent value="yearly">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
-            {[0, 1, 2, 4].map((item) => (
-              <Card key={item}>
-                <CardHeader>
-                  <CardTitle className="text-lg font-medium">
-                    Organizations
-                  </CardTitle>
-                  <CardDescription>
-                    <div className="mt-4">
-                      <p className="text-2xl font-semibold">$100</p>
-                      <p className="text-xs text-gray-500">Per Month/user</p>
+          <Card className="border-[#7877c6] border-2 max-w-[500px] mx-auto inset-0 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.1),rgba(255,255,255,0))] dark:bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+            <CardHeader>
+              <CardTitle className="text-xl font-medium">
+                {cardData[1].title} <span className="ms-2 px-3 py-1 bg-primary text-white rounded-full text-xs font-bold">🔥 Most Popular</span>
+              </CardTitle>
+              <CardDescription>
+                <div className="mt-4">
+                  <p className="text-2xl font-semibold">{cardData[1].price}</p>
+                  <p className="text-xs text-gray-500">
+                    {cardData[1].duration}
+                  </p>
+                </div>
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="px-6">
+              <div>
+                <div className="space-y-3 mt-4 text-gray-700 dark:text-gray-300 font-light">
+                  {cardData[1].features.map((feature, index) => (
+                    <div key={index}>
+                      <p className="flex items-center gap-1.5 font-medium">
+                        <BadgeCheck className="h-5 w-5" />
+                        {feature.title}
+                      </p>
+                      {feature.description && (
+                        <p className="text-sm pl-6 pt-0.5">
+                          {feature.description}
+                        </p>
+                      )}
                     </div>
-                  </CardDescription>
-                </CardHeader>
-                <CardContent className="px-6">
-                  <div>
-                    <p className="text-sm font-medium">
-                      Great for large businesses
-                    </p>
-                    <div className="space-y-3 mt-4">
-                      <p className="text-sm flex items-center gap-1.5">
-                        <BadgePercent className="h-5 w-5" />
-                        Great for large businesses
-                      </p>
-                      <p className="text-sm flex items-center gap-1.5">
-                        <BadgePercent className="h-5 w-5" />
-                        Great for large businesses
-                      </p>
-                      <p className="text-sm flex items-center gap-1.5">
-                        <BadgePercent className="h-5 w-5" />
-                        Great for large businesses
-                      </p>
-                      <p className="text-sm flex items-center gap-1.5">
-                        <BadgePercent className="h-5 w-5" />
-                        Great for large businesses
-                      </p>
-                      <p className="text-sm flex items-center gap-1.5">
-                        <BadgePercent className="h-5 w-5" />
-                        Great for large businesses
-                      </p>
-                    </div>
-                  </div>
-                </CardContent>
-                <CardFooter className="mt-4">
-                  <Button className="w-full">Get started</Button>
-                </CardFooter>
-              </Card>
-            ))}
-          </div>
+                  ))}
+                </div>
+              </div>
+            </CardContent>
+            <CardFooter className="mt-4">
+              <Button className="w-full">{cardData[1].buttonText}</Button>
+            </CardFooter>
+          </Card>
         </TabsContent>
       </Tabs>
     </div>
